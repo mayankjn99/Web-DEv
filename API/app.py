9f28c7472e62a3b5589fb5e07804594d7b14a3e8
mayankjn99 <jn.mayank99@gmail.com>
2024-04-21 19:25:31 +0530

    Added welcome api

--- API/app.py
+++ API/app.py
@@ -10,6 +10,10 @@ class User(BaseModel):
     age: int
     email: str
 
+@app.get("/")
+def welcome_app():
+    return "Api is what we got"
+
 @app.post("/users/{user_id}")
 def create_user(user_id: str, user: User):
     if user_manager.get_user(user_id):
@@ -22,3 +26,4 @@ def get_user(user_id: str):
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return user
+
