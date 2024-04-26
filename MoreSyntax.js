ed50206e5f3d207f51d35970ced88c5b65c7e4d4
mayankjn99 <jn.mayank99@gmail.com>
2024-04-27 02:19:16 +0530

    Added utility functions of js

---
+++ MoreSyntax.js
@@ -0,0 +1,43 @@
+function* iteratorFunc() {
+    let count = 0;
+    for (let i = 0; i < 2; i++) {
+        count++;
+        yield i;
+    }
+    return count;
+  }
+  
+  let iterator = iteratorFunc();
+  console.log(iterator.next()); // {value:0,done:false}
+  console.log(iterator.next()); // {value:1,done:false}
+  console.log(iterator.next()); // {value:2,done:true}
+// done true means generator func has reached to return statement 
+
+const newSet = new Set([4, 5, 6, 7]);
+console.log(newSet);// Outputs Set {4,5,6,7}
+
+// const newSet2 = new WeakSet([3, 4, 5]); //Throws an error
+
+
+let obj1 = {message:"Hello world"};
+const newSet3 = new WeakSet([obj1]);
+console.log(newSet3.has(obj1)); // true
+
+const map1 = new Map();
+map1.set('Value', 1);
+
+const map2 = new WeakMap();
+// map2.set('Value', 2.3); // Throws an error
+
+let obj = {name:"Vivek"};
+const map3 = new WeakMap();
+map3.set(obj, {age:23});
+
+const classDetails = {
+    strength: 78,
+    benches: 39,
+    blackBoard:1
+  }
+  
+  const {strength, benches,blackBoard} = classDetails;
+  console.log(strength, benches,blackBoard); // 78 39 1
