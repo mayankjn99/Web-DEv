d5bb0d49930b2cd273b3d0a9d0da0ff1adf82fc9
mayankjn99 <jn.mayank99@gmail.com>
2024-05-03 17:47:29 +0530

    Initial commit

---
+++ ToDo/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import AddItem from "./components/AddItem"
+import  TodoList from "./components/TodoList"
+import './App.css'
+
+function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodos = (text) => {
+    // console.log('item', text)
+    const newTodo = { id: Date.now(), text };
+     setTodos([...todos, newTodo]);
+
+  }
+
+  // console.log('todos', todos)
+  return (
+    <div className="root">
+      <AddItem addTodos={addTodos}/>
+      <TodoList todos={todos}/>
+    </div>
+    
+  )
+}
+
+export default App
