60b86577e4b07549fede111dcfeca8f9ff70c0c0
mayankjn99 <jn.mayank99@gmail.com>
2024-05-06 02:48:04 +0530

    App Setup for routing

--- ToDo/src/App.jsx
+++ ToDo/src/App.jsx
@@ -1,24 +1,18 @@
-import { useState } from "react"
-import AddItem from "./components/AddItem"
-import  TodoList from "./components/TodoList"
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Todo from './Todo'
+import SearchBar from './components/PaginatedSearch'
 
 function App() {
-  const [todos, setTodos] = useState([])
 
-  const addTodos = (text) => {
-    // console.log('item', text)
-    const newTodo = { id: Date.now(), text };
-     setTodos([...todos, newTodo]);
-
-  }
 
   // console.log('todos', todos)
   return (
-    <div className="todo-app">
-      <AddItem addTodos={addTodos}/>
-      <TodoList todos={todos}/>
-    </div>
+    <Routes>
+      <Route path='/' element={<Todo/>}/>
+      <Route path='/search-bar' element={<SearchBar/>}/>
+    </Routes>
+
     
   )
 }
