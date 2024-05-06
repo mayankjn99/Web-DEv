ff4f2cf161e263cf36bf114ae5035583a61946f4
mayankjn99 <jn.mayank99@gmail.com>
2024-05-06 23:00:40 +0530

    Added Audio Player

--- ToDo/src/App.jsx
+++ ToDo/src/App.jsx
@@ -3,9 +3,11 @@ import { Routes, Route } from 'react-router-dom'
 import Todo from './Todo'
 import SearchBar from './components/PaginatedSearch'
 import Tree from './components/Tree'
+import AudioPlayer from './components/AudioPlayer'
+import { useState } from 'react'
 
 function App() {
-
+  const [audioSrc, setAudioSrc] = useState('');
 
   // console.log('todos', todos)
   return (
@@ -13,6 +15,7 @@ function App() {
       <Route path='/' element={<Todo/>}/>
       <Route path='/search-bar' element={<SearchBar/>}/>
       <Route path='/tree-structure' element={<Tree/>}/>
+      <Route path='/audio-player' element={<AudioPlayer audioSrc={audioSrc} setAudioSrc={setAudioSrc}/>}/>
     </Routes>
 
     
